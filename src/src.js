866619562663d897b74bc436f6027f8605fff62c
Delon Pratama <delonpratama1009@gmail.com>
2025-02-16 20:49:49 +0700

    updated logo

--- src/src.js
+++ src/src.js
@@ -2,16 +2,16 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("skills", () => ({
     items: [
-      { id: 1, name: "Laravel", img: "laravel.svg" },
-      { id: 2, name: "CodeIgniter", img: "codeigniter.svg" },
-      { id: 3, name: "Bootstrap", img: "bootstrap.svg" },
-      { id: 4, name: "JavaScript", img: "javascript.svg" },
-      { id: 5, name: "Alpine.js", img: "alpinejs.svg" },
-      { id: 6, name: "PHP", img: "php.svg" },
-      { id: 7, name: "SQL", img: "sql.svg" },
-      { id: 8, name: "Git", img: "git.svg" },
-      { id: 9, name: "GitHub", img: "github.svg" },
-      { id: 10, name: "Figma", img: "figma.svg" },
+      { name: "Laravel", img: "laravel.svg" },
+      { name: "CodeIgniter", img: "codeigniter.svg" },
+      { name: "Bootstrap", img: "bootstrap.svg" },
+      { name: "JavaScript", img: "javascript.svg" },
+      { name: "Alpine.js", img: "alpinejs.svg" },
+      { name: "PHP", img: "php.svg" },
+      { name: "MySQL", img: "mysql.svg" },
+      { name: "SQL", img: "sql.svg" },
+      { name: "Git", img: "git.svg" },
+      { name: "Figma", img: "figma.svg" },
     ],
   }));
 });
@@ -21,7 +21,6 @@ document.addEventListener("alpine:init", () => {
   Alpine.data("experiences", () => ({
     items: [
       {
-        id: 1,
         name: "Web Development Projects",
         date: "2023 - 2024",
         desc: "During my college studies, I worked on several web development projects that showcase my skills in front-end and back-end development.",
