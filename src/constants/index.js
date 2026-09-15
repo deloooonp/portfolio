dc902b2c361db146d88bcd74d905671c18847b0a
Delon Pratama <146929947+deloooonp@users.noreply.github.com>
2026-09-15 14:24:59 +0700

    removed gallery for now (#18)

--- src/constants/index.js
+++ src/constants/index.js
@@ -48,12 +48,12 @@ export const DOCK_APPS = [
     icon: "safari.png",
     canOpen: true,
   },
-  {
-    id: "photos",
-    name: "Gallery", // was "Photos"
-    icon: "photos.png",
-    canOpen: true,
-  },
+  // {
+  //   id: "photos",
+  //   name: "Gallery", // was "Photos"
+  //   icon: "photos.png",
+  //   canOpen: true,
+  // },
   {
     id: "contact",
     name: "Contact", // or "Get in touch"
